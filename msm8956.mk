ee80aed1b56b10bd4cad11e7c3c45fe17f7a7c73
xyyx <xyyx@mail.ru>
2016-12-26 19:01:01 +0800

    kenzo: Integrate Light lib
    Change-Id: I459d785630490b4a8e1adc342253b39b9569c094

--- msm8956.mk
+++ msm8956.mk
@@ -27,6 +27,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
 
 # Permissions
