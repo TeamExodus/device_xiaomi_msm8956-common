9af5bde5c80fa058a25a09bd5da6dd9bb3173eb2
TheStrix <parthbhatia@lineageos.org>
2017-02-10 16:30:33 +0530

    msm8956: Re-enable XML audio policy
    Change-Id: Icb30d1ec65c36033b1bec9fa7b78f5872514ba92
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -90,7 +90,15 @@ PRODUCT_COPY_FILES += \
     device/xiaomi/msm8956-common/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     device/xiaomi/msm8956-common/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     device/xiaomi/msm8956-common/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    device/xiaomi/msm8956-common/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+# XML Audio configuration files
+PRODUCT_COPY_FILES += \
+    device/xiaomi/msm8956-common/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
 # Browser
 PRODUCT_PACKAGES += \
